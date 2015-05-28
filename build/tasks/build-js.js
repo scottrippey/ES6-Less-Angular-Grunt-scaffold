4e5e55761b70ddbf15fce72983f995afa98828ff
Scott Rippey <scott.william.rippey@gmail.com>
2015-05-27 23:12:27 -0700

    [ES6] Have to combine manually before compiling a ES6 file, because Babel does not support multiple sources

--- build/tasks/build-js.js
+++ build/tasks/build-js.js
@@ -1,20 +1,22 @@
 module.exports = function(grunt) {
-	grunt.registerTask('build-js', [ 'babel:ALL-ES6' ]);
+	grunt.registerTask('build-js', [ 'concat:ANGULAR-MODULES-ES6', 'babel:ANGULAR-MODULES-ES6' ]);
 	
 	grunt.config.merge({
+		concat: {
+			'ANGULAR-MODULES-ES6': {
+				src: 'src/angular-modules/**/*.js',
+				dest: 'dist/js/angular-modules.combined.es6.js'
+			}
+		},
 		babel: {
-			'ALL-ES6': {
-				files: [
-					{ 
-						src: 'src/angular-modules/**/*.es6.js',
-						dest: 'dist/js/angular-modules.combined.js'
-					}
-				]
+			'ANGULAR-MODULES-ES6': {
+				src: 'dist/js/angular-modules.combined.es6.js',
+				dest: 'dist/js/angular-modules.combined.js'
 			}
 		},
 		watch: {
 			'BUILD-JS': {
-				files: [ 'src/angular-modules/**/*.es6.js' ],
+				files: [ 'src/angular-modules/**/*.js' ],
 				tasks: [ 'build-js' ]
 			},
 			'ALL-ES6-livereload': {
---
+++ dist/js/angular-modules.combined.es6.js
@@ -0,0 +1,13 @@
+angular.module('MainApp', [
+	'ngAnimate',
+	'ngTouch'
+]);
+(function() {
+	
+	var items = [ "one", "two", "three" ];
+	for (let item of items) {
+		console.log(item);
+	}
+	
+})();
+//# sourceMappingURL=angular-modules.combined.es6.js.map
